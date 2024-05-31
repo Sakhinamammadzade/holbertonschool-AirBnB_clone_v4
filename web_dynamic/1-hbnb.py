eae1dd227a5c4e4c0122b9ae8d92cc9f3c136ddd
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-06-01 00:27:55 +0400

    some changes

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -34,13 +34,12 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-    cache_id = str(uuid.uuid4())
     
     return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id)
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
